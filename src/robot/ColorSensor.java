4b1ccc10daddab2fcdeab3c756fb8e1988aac024
Ville Kautonen <ville.kille@hotmail.com>
2017-10-02 13:48:32 +0300

    Added ColorSensor class and modified Main
    Added a class for the ColorSensor and modified Main for behavior and arbitrator implementation.

---
+++ src/robot/ColorSensor.java
@@ -0,0 +1,97 @@
+package robot;
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+
+public class ColorSensor {
+
+	private Port sensorPort;
+	private SensorModes rgbSensor;
+	private SampleProvider colorProvider;
+	private float[] rgbSample;
+	private float[] colorA;
+	private float[] colorB;
+
+//	Class constructor with a String parameter for configuring the port.
+	public ColorSensor(String port) {
+		sensorPort = LocalEV3.get().getPort(port);
+		rgbSensor = new EV3ColorSensor(sensorPort);
+		colorProvider = ((EV3ColorSensor)rgbSensor).getRGBMode();
+		rgbSample = new float[colorProvider.sampleSize()];
+		colorA = new float[colorProvider.sampleSize()];
+		colorB = new float[colorProvider.sampleSize()];
+		saveColors();
+
+	}
+
+//	checkColor compares the color sensor's current input at
+//	the given time to the sample provided by saveColors method.
+//	The method returns true, when the calculated value is
+//	lower than the threshold value. Otherwise returns false.
+	public boolean checkColor () {
+		colorProvider.fetchSample(rgbSample, 0);
+		int[] realA = new int[3];
+        realA[0] = Math.round(colorA[0]*765);
+        realA[1] = Math.round(colorA[1]*765);
+        realA[2] = Math.round(colorA[2]*765);
+
+		int[] realB = new int[3];
+        realB[0] = Math.round(colorB[0]*765);
+        realB[1] = Math.round(colorB[1]*765);
+        realB[2] = Math.round(colorB[2]*765);
+
+        int[] sampleRGB = new int[3];
+        sampleRGB[0] = Math.round(rgbSample[0]*765);
+        sampleRGB[1] = Math.round(rgbSample[1]*765);
+        sampleRGB[2] = Math.round(rgbSample[2]*765);
+
+        double sampleColorA;
+        int aR = realA[0] - sampleRGB[0];
+        int aG = realA[1] - sampleRGB[1];
+        int aB = realA[2] - sampleRGB[2];
+        sampleColorA = Math.sqrt((aR*aR) + (aG*aG) + (aB*aB));
+
+        double sampleColorB;
+        int bR = realB[0] - sampleRGB[0];
+        int bG = realB[1] - sampleRGB[1];
+        int bB = realB[2] - sampleRGB[2];
+        sampleColorB = Math.sqrt((bR*bR) + (bG*bG) + (bB*bB));
+
+        int thresholdValue = 50;
+        if (thresholdValue > sampleColorA) {
+                return true;
+        }
+        return false;
+	}
+//	saveColors sets up the sample colors for the colorCheck
+//	method.
+	public void saveColors() {
+
+    	System.out.println("Show samplecolor A to the color sensor and press Escape to verify it.");
+
+
+    	while (Button.ID_ESCAPE == Button.waitForAnyPress()) {
+        	colorProvider.fetchSample(colorA, 0);
+        	System.out.println(Math.round(colorA[0]*765));
+        	System.out.println(Math.round(colorA[1]*765));
+        	System.out.println(Math.round(colorA[2]*765));
+        	System.out.println("Press any key to continue.");
+    	}
+
+    	System.out.println("");
+    	System.out.println("Show samplecolor B to the color sensor and press Escape to verify it.");
+    	while (Button.ID_ESCAPE == Button.waitForAnyPress()) {
+   		 colorProvider.fetchSample(colorB, 0);
+        	System.out.println(Math.round(colorB[0]*765));
+        	System.out.println(Math.round(colorB[1]*765));
+        	System.out.println(Math.round(colorB[2]*765));
+        	System.out.println("Press any key to continue.");
+    	}
+
+    }
+
+}
+
--- src/robot/Main.java
+++ src/robot/Main.java
@@ -1,8 +1,21 @@
 package robot;
 
+import lejos.robotics.subsumption.*;
+
 public class Main {
 	public static void main(String[] args) {
 		Motor motor = new Motor();
 		motor.test();
+
+//		Behavior's priority is determined by it's position in
+//		behaviorArray. First index being the lowest priority
+//		and last being the highest.
+
+//		Behavior b1 = new BUser();
+//		Behavior b2 = new BDemo();
+//		Behavior b3 = new BMenu();
+//		Behavior b4 = new BExit();
+//		Behavior[] behaviorArray = {b1, b2, b3, b4};
+//		Arbitrator arbi = new Arbitrator(behaviorArray);
 	}
 }
