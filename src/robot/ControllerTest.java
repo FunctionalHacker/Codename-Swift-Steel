7b29dc2b43ff0542e6bec3670ddb5374e93d2209
Marko Korhonen <reekymarko@reekynet.com>
2017-10-09 13:43:03 +0300

    Moar testing

--- src/robot/ControllerTest.java
+++ src/robot/ControllerTest.java
@@ -19,16 +19,17 @@ public class ControllerTest {
 
 		while(running){
 			buttons = psp.getButtons();
+			try {
 			if(buttons[0] == 1){
 				bMotor.forward();
+			}
+			else if(buttons[0] == 0 && bMotor.getRotationSpeed() > 0){
+				bMotor.stop();
 			}
 			if(buttons[2] == 1){
 				bMotor.backward();
 			}
-			if(buttons[0] == 0){
-				bMotor.stop();
-			}
-			if(buttons[2] == 0){
+			else if(buttons[2] == 0 && bMotor.getRotationSpeed() < 0){
 				bMotor.stop();
 			}
 			if(buttons[1] == 1){
