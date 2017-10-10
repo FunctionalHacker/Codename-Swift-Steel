fbf28b2a4b6b1cfaf8bd743c563196abeae33e56
Marko Korhonen <reekymarko@reekynet.com>
2017-10-10 13:08:47 +0300

    Changed to 2 tire stacks

--- src/robot/Navigator.java
+++ src/robot/Navigator.java
@@ -6,10 +6,10 @@ public class Navigator {
 	TopMotor tMotor;
 
 	private int rideHeight = 70*3;
-	private int t1Height = rideHeight - 23*3;
+	private int t1Height = rideHeight - 13*3;
+	private int t2Height = rideHeight - 23*3;
 	private int tA = 140*3;
 	private int tB = 100*3;
-	private int tC = 60*3;
 	private int cA = 260*3;
 	private int cB = 210*3;
 	private int cC = 160*3;
@@ -32,27 +32,27 @@ public class Navigator {
 		mMotor.rotate(-t1Height);
 	}
 
-	public void tireC1(){
-		mMotor.rotate((rideHeight)-mMotor.getCurRotation());
-		bMotor.rotate((tC)-bMotor.getCurRotation());
-		mMotor.rotate(-t1Height);
-	}
-
 	public void tireA2(){
 		mMotor.rotate((rideHeight)-mMotor.getCurRotation());
 		bMotor.rotate((tA)-bMotor.getCurRotation());
-		mMotor.rotateToDefaultPos();
+		mMotor.rotate(-t2Height);
 	}
 
 	public void tireB2(){
 		mMotor.rotate((rideHeight)-mMotor.getCurRotation());
 		bMotor.rotate((tB)-bMotor.getCurRotation());
+		mMotor.rotate(-t2Height);
+	}
+
+	public void tireA3(){
+		mMotor.rotate((rideHeight)-mMotor.getCurRotation());
+		bMotor.rotate((tA)-bMotor.getCurRotation());
 		mMotor.rotateToDefaultPos();
 	}
 
-	public void tireC2(){
+	public void tireB3(){
 		mMotor.rotate((rideHeight)-mMotor.getCurRotation());
-		bMotor.rotate((tC)-bMotor.getCurRotation());
+		bMotor.rotate((tB)-bMotor.getCurRotation());
 		mMotor.rotateToDefaultPos();
 	}
 
